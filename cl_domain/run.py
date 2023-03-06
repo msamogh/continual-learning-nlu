64d6bd9a414f0a4a43c72edb50930e950a1435d3
Amogh Mannekote <msamogh@gmail.com>
2023-03-06 09:35:05 -0500

    Refactoring

--- cl_domain/run.py
+++ cl_domain/run.py
@@ -1,25 +1,27 @@
 from cl_domain.config import get_args
 from cl_domain.domain import Domain, DomainSplit
-from cl_domain.experiment import ORDERINGS, ExperimentRun
+from cl_domain.experiment import ORDERINGS, CLRunInput
 
 
-def generate_experiment_run(args):
-    domains = Domain.get_all_domains(args["ctx_window_size"])
+def generate_experiment_input(args):
+    domains = Domain.generate_samples(args["ctx_window_size"])
     ordering_fn = ORDERINGS[args["ordering"]]
     ordering = ordering_fn(domains)
-    subsamples = [
-        DomainSplit.subsample(domain, args["train_size_per_domain"])
+    subsamples = {
+        domain: DomainSplit.subsample(domain, args["train_size_per_domain"])
         for domain in ordering
-    ]
-    experiment_run = ExperimentRun(
+    }
+    return CLRunInput(
         domain_ordering=ordering,
-        domain_samples=subsamples
+        domain_wise_samples=subsamples
     )
-    return experiment_run
+
+
+def run_training_experiment(cl_run_input: CLRunInput):
+    pass
 
 
 if __name__ == "__main__":
     args = get_args()
-    experiment_run = generate_experiment_run(args)
+    experiment_run = generate_experiment_input(args)
     print("Done")
-
