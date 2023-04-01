3bdf5a669c11c66fcc6e55778afdb65fa16133a0
lrnfreek <lrnfreek@gmail.com>
2023-04-01 05:19:15 -0400

    Added min_sum_path, max_sum_path, will consider only 10 domains since tsp bruteforce takes time

--- cl_domain/domain_similarity/distance.py
+++ cl_domain/domain_similarity/distance.py
@@ -1,6 +1,12 @@
 from sklearn.metrics.pairwise import euclidean_distances
+from typing import *
+import math
 
 
-def euclidean_distance(a, b):
-    return euclidean_distances([a], [b])[0][0]
+def euclidean_distance(x: List[float], y: List[float]) -> float:
+    return math.sqrt(sum((xi - yi) ** 2 for xi, yi in zip(x, y)))
 
+
+DISTANCE = {
+    "eud": euclidean_distance
+}
