dfd911420f6c6eedcfeb4172b1117e4576c206ad
Amogh Mannekote <msamogh@gmail.com>
2023-04-06 19:57:09 -0400

    Increase epochs and batch size

--- cl_domain/train/__init__.py
+++ cl_domain/train/__init__.py
@@ -17,9 +17,9 @@ TOKENIZER = T5Tokenizer.from_pretrained(MODEL_NAME)
 def get_training_args(args: Dict[Text, Any]):
     return TrainingArguments(
         output_dir='./results',
-        num_train_epochs=1,
-        per_device_train_batch_size=8,
-        per_device_eval_batch_size=8,
+        num_train_epochs=5,
+        per_device_train_batch_size=32,
+        per_device_eval_batch_size=32,
         warmup_steps=500,
         weight_decay=0.01,
         logging_dir='./logs',
